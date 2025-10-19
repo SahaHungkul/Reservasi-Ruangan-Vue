3a2462592d8fc680de0fca45168313d0389df612
SahaHungkul <mhasbysyabani27@gmail.com>
2025-10-19 18:55:19 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1> Ini Home</h1>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup></script>
+
+<template>
+  <h1 class="text-blue-900">Ini testing</h1>
+</template>
+
+<style></style>
